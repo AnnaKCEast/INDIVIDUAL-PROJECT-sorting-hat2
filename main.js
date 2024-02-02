0cc77203490873ccbcaa5543b197649b8ec70299
Anna East <annakceast@yahoo.com>
2024-02-01 19:47:14 -0600

    Made a multiple additions and switched things over from HTML to JS.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDivID = document.querySelector(divId);
+  selectedDivID.innerHTML = htmlToRender;
+};
 // Data of The Fellowship
 const fellowship = [
   {
@@ -33,6 +37,7 @@ const fellowship = [
     orcArmy: false,
   },
 ];
+
 // Orc Army Data
 const urukHai = [
   {
@@ -44,13 +49,11 @@ const urukHai = [
 ];
 
 // Saruman's Army array
-const sarumansArmy = [];
+// const sarumansArmy = [];
 
 // Code for rendering to DOM so HTML and JS can connect
-const renderToDom = (divId, htmlToRender) => {
-  const selectedDivID = document.querySelector(divId);
-  selectedDivID.innerHTML = htmlToRender;
-};
+
+
 
 // forEach to loop through all the travelers
 const cardsOnDom = (fellowship) => {
@@ -61,14 +64,52 @@ const cardsOnDom = (fellowship) => {
          <h3 class="card-title">Name: ${traveler.name}</h3>
          <h5 class="card-subtitle mb-2 text-body-secondary">I am a/an: ${
            traveler.type}</h5>
-         <p class="card-text">Are you an Orc by chance? ${
+         <p class="card-text">Are you an Orc? ${
            traveler.orcArmy ? "I serve the Dark Lord" : "Sauron MUST be destroyed"}</p>
          <button class="btn btn-dark" id="delete--${traveler.id}--">Traitor!</button>
        </div>
      </div>`;
   });
-  renderToDom("#app", domString);
+  renderToDom("#theFellowship", domString);
 };
+
+const armyOnDom = (urukHai) => {
+  let domString = "";
+  orcArmy.forEach((orc) => {
+    domString += `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+      <h3 class="card-title">Name ${orc.name}</h3>
+      <p class="card-text">Are you an Orc? ${
+        orc.orcArmy ? "I serve the Dark Lord" : "Sauron MUST be destroyed"}</p>
+      </div>
+    </div>`
+  })
+  renderToDom ("#sauronArmy", domString)
+}
+
+
+const hobbitForm = document.querySelector("#hobbitForm");
+// ****Starting the form by calling it from the document****
+const createNewMember = (e) => {
+  e.preventDefault();
+
+  const createFellowObj = {
+    id: fellowship.length + 1,
+    name: document.querySelector("#name").value,
+    type: type [Math.floor(Math.random() * type.length)],
+    id: Math.floor(Math.random() * 100) +1
+  };
+      hobbitForm.addEventListener("submit", (e) => {       
+    createNewMember(e);
+    fellowButtons();
+    renderToDom ("#app", domString)
+});
+  // Pushing new addition card
+  fellowship.push(createFellowObj);
+    cardsOnDom(fellowship);
+    hobbitForm.reset();
+  }  
+
 
 // Using filter to create new arrays for each house
 const filter = (travelerType) => {
@@ -87,7 +128,7 @@ const filter = (travelerType) => {
 
 // Buttons to show after the initial
 const fellowButtons = () => {
-  domString = `<button type="button" class="btn btn-danger" id="man">Time of Man</button>
+  let domString = `<button type="button" class="btn btn-danger" id="man">Time of Man</button>
     <button type="button" class="btn btn-primary" id="elf">Time of the Elves</button>
     <button type="button" class="btn btn-warning" id="hobbit">Tricksy Hobbitses</button>
     <button type="button" class="btn btn-success" id="wizard">Wise Wizards</button>
@@ -97,85 +138,36 @@ const fellowButtons = () => {
   renderToDom("#theGreatBtn", domString);
 };
 
-
-// ****Starting the form by calling it from the document****
-// const formContainer = document.querySelector('formContainer');
-
-// `<div class="col-md-4">
-//         <label for="validationCustom" id="hobbitForm">Who goes there? ${traveler.name}</label>
-//         <input type="text" class="form-control" id="validationCustom" value="" required>
-//       </div>
-//       <div class="col-12">
-//         <button type="submit" class="btn btn-info" id="sortHobbit" >Tell Me! </button>
-//       </div>`;
-
-// Function to pull element
-const createNewFollower = (e) => {
-  e.preventDefault();
-
-  //  Adding a new card with values
-  const createFellowObj = {
-    id: fellowship.length + 1,
-    name: document.querySelector("#name").value,
-    type: document.querySelector("#type").value,
-  };
-  // Pushing new addition card
-  fellowship.push(createFellowObj);
-  cardsOnDom(fellowship);
-  form.reset();
-};
-// submit button even listener for the createNewFollower card
-
-
-
-
-
-// Event Listeners
-//  Welcome button, form, and filter selectors
-const advButton = document.querySelector("#advButton");
-const hobbitForm = document.querySelector("form");
 const theGreatBtn = document.querySelector("#theGreatBtn");
-// const form = document.querySelector("form");
 
 
-advButton.addEventListener("click", hobbitForm);
-
-
-hobbitForm.addEventListener("submit", (e) => {
-  createNewFollower(e);
-  fellowButtons();
-});
-
-// form.addEventListener("submit", createNewFollower);
-// const app = document.querySelector("#theFellowship");
-
-theGreatBtn.addEventListener("click", (e) => {
+ theGreatBtn.addEventListener("click", (e) => {
   console.log("for the love CLICK!!!!!!")
   if (e.target.id.includes("man")) {
     const filterMan = fellowship.filter((traveler) => traveler.type === "man");
     cardsOnDom(filterMan);
   }
-  if (e.target.id.includes("elf")) {
+  else if (e.target.id.includes("elf")) {
     const filterElf = fellowship.filter((traveler) => traveler.type === "elf");
     console.log("Legolas");
     cardsOnDom(filterElf);
   }
-  if (e.target.id.includes("hobbit")) {
+  else if (e.target.id.includes("hobbit")) {
     const filterHobbit = fellowship.filter((traveler) => traveler.type === "hobbit");
     console.log("Master Frodo");
     cardsOnDom(filterHobbit);
   }
-  if (e.target.id.includes("wizard")) {
+  else if (e.target.id.includes("wizard")) {
     const filterWizard = fellowship.filter((traveler) => traveler.type === "wizard");
     console.log("FLY you fools!");
     cardsOnDom(filterWizard);
   }
-  if (e.target.id.includes("dwarf")) {
+  else if (e.target.id.includes("dwarf")) {
     const filterDwarf = fellowship.filter((traveler) => traveler.type === "dwarf");
     console.log("Nobody tosses a dwarf");
     cardsOnDom(filterDwarf);
   }
-  if (e.target.id.includes("all")) {
+  else if (e.target.id.includes("all")) {
     cardsOnDom(fellowship);
   }
 });
@@ -187,3 +179,13 @@ app.addEventListener("click", (e) => {
     console.log("exiled");
   }
 });
+
+
+// Attempting to splice the cards into new arrays for the orc army
+// const app = document.querySelector("#sauronArmy")
+// app.addEventListener("click" (e) => {
+//   if (e.target.id.includes("#orcArmy"))
+//   console.log("In the army now")
+// })
+
+//  Destruct the array
